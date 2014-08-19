f8e045471b8d1ca1fb93d313682944258558a069
sgardine <sgardine@bez>
2014-08-18 22:01:29 -0400

    basic query exact match

--- src/edu/cmu/mixer/wrapup/WrapupQuery.java
+++ src/edu/cmu/mixer/wrapup/WrapupQuery.java
@@ -1,6 +1,7 @@
 package edu.cmu.mixer.wrapup;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class WrapupQuery extends javax.servlet.http.HttpServlet {
   public void doGet(javax.servlet.http.HttpServletRequest req,
@@ -18,13 +19,37 @@ public class WrapupQuery extends javax.servlet.http.HttpServlet {
       resp.setHeader("Access-Control-Max-Age", "3600");
       resp.setHeader("Access-Control-Allow-Headers", "x-requested-with");
     }
-
+    
     try {
       String callback = req.getParameter("callback");     
 
+      org.json.JSONArray wrappers = new JSONArray();
       org.json.JSONObject json = new JSONObject();
       
       json.putOpt("url", req.getParameter("url"));
+      json.putOpt("wrappers", wrappers);
+
+      System.err.println("QUERY: " + json.toString());
+
+      if (req.getParameter("signature") != null) {
+        org.json.JSONArray bysig = queryBySignature(req.getParameter("signature"));
+        for (int i = 0; i < bysig.length(); i++) {
+          wrappers.put(bysig.opt(i));
+        }
+      }
+
+      if (req.getParameter("url") != null) {
+        org.json.JSONArray byurl = queryByURL(req.getParameter("url"));
+        for (int i = 0; i < byurl.length(); i++) {
+          wrappers.put(byurl.opt(i));
+        }
+        
+      }
+
+      if (wrappers.length() == 0) {
+        resp.setStatus(resp.SC_NO_CONTENT);
+      }
+      
 
       if (callback == null) {
         resp.setContentType("application/json");
@@ -40,5 +65,43 @@ public class WrapupQuery extends javax.servlet.http.HttpServlet {
       resp.setContentType("text/plain");
       ex.printStackTrace(resp.getWriter());
     }
+  }
+
+  protected static com.google.appengine.api.datastore.DatastoreService ds =
+    com.google.appengine.api.datastore.DatastoreServiceFactory.getDatastoreService();
+
+  public static org.json.JSONArray queryBySignature(String signature) {
+    // PLACEHOLDER; NOT YET IMPLEMENTED
+    return new org.json.JSONArray();
+  }
+  public static org.json.JSONArray queryByURL(String url) {
+    org.json.JSONArray results = new JSONArray();
+            
+    com.google.appengine.api.datastore.Query query = 
+      new com.google.appengine.api.datastore.Query("Wrapper");
+    com.google.appengine.api.datastore.Query.FilterPredicate filter =
+      new com.google.appengine.api.datastore.Query.FilterPredicate("url",
+                                                                   com.google.appengine.api.datastore.Query.FilterOperator.EQUAL,
+                                                                   url);
+    query = query.setFilter(filter);
+
+    com.google.appengine.api.datastore.PreparedQuery pq = 
+      ds.prepare(query);
+    for (com.google.appengine.api.datastore.Entity result : pq.asIterable()) {
+      
+      String wrapperString = ((com.google.appengine.api.datastore.Text) result.getProperty("wrapper")).getValue();
+      try {
+        results.put(new org.json.JSONArray(wrapperString));
+      } catch (Exception ex) {
+        try {
+          results.put(new org.json.JSONObject(wrapperString));
+        } catch (Exception ex2) {
+          ex2.printStackTrace(System.err);
+        }
+      }
+
+    }
+    
+    return results;
   }
 }
