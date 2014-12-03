c662725a44a953fb338f974e43d7c91a3c82b256
sgardine <sgardine@bez>
2014-12-03 01:04:07 -0500

    access stuff

--- src/edu/cmu/mixer/access/EventLog.java
+++ src/edu/cmu/mixer/access/EventLog.java
@@ -33,6 +33,7 @@ public class EventLog {
       entity.setProperty("taskno", event.optString("taskno", ""));
       entity.setProperty("condition", event.optString("condition", ""));
       entity.setProperty("sessionid", event.optString("sessionid", ""));
+      entity.setProperty("hash", event.optString("hash", ""));
       
       ds.put(entity);
       
@@ -41,5 +42,116 @@ public class EventLog {
       ex.printStackTrace(System.err);
     }
     return response;
+  }
+
+  private org.json.JSONObject getTaskOrders() throws Exception {
+    org.json.JSONObject taskorders = new org.json.JSONObject();
+    com.google.appengine.api.datastore.Query query = 
+      new com.google.appengine.api.datastore.Query("TaskOrder");
+    com.google.appengine.api.datastore.PreparedQuery pq = ds.prepare(query);
+    for (com.google.appengine.api.datastore.Entity result : pq.asIterable()) {
+      String key = result.getKey().toString();
+      taskorders.putOpt(key, new org.json.JSONArray(result.getProperty("ordering").toString()));
+    }
+    
+    return taskorders;
+  }
+
+  private org.json.JSONObject getCondOrders() throws Exception {
+    org.json.JSONObject condorders = new org.json.JSONObject();
+    com.google.appengine.api.datastore.Query query = 
+      new com.google.appengine.api.datastore.Query("CondOrder");
+    com.google.appengine.api.datastore.PreparedQuery pq = ds.prepare(query);
+    for (com.google.appengine.api.datastore.Entity result : pq.asIterable()) {
+      String key = result.getKey().toString();
+      condorders.putOpt(key, new org.json.JSONArray(result.getProperty("ordering").toString()));
+    }
+    
+    return condorders;
+  }
+  
+  private org.json.JSONObject orderingCache = null;
+  public org.json.JSONObject drawOrderings(org.json.JSONObject info) throws Exception {
+    if (orderingCache == null) {
+      orderingCache = new org.json.JSONObject();
+
+      orderingCache.putOpt("taskorders", this.getTaskOrders());
+      orderingCache.putOpt("condorders", this.getCondOrders());
+
+    }
+    System.err.println("ORDERING CACHE: " + orderingCache.toString(2));
+
+    if (orderingCache.has(info.optString("sessionid"))) {
+      return orderingCache.optJSONObject(info.optString("sessionid"));
+    }
+    
+    org.json.JSONObject counts = new org.json.JSONObject();
+    /*
+      taskorders.forEach(function(to) { counts[to.id] = 0; }
+      condorders.forEach(function(co) { counts[co.id] = 0; }      
+     */
+
+    com.google.appengine.api.datastore.Query query = 
+      new com.google.appengine.api.datastore.Query("OrderStatus");
+    com.google.appengine.api.datastore.PreparedQuery pq = ds.prepare(query);
+    for (com.google.appengine.api.datastore.Entity result : pq.asIterable()) {
+      String taskkey = result.getProperty("taskorderid").toString();
+      if (taskkey != null) {
+	counts.putOpt(taskkey, 1+counts.optInt(taskkey, 0));
+      }
+      String condkey = result.getProperty("condorderid").toString();
+      if (condkey != null) {
+	counts.putOpt(condkey, 1+counts.optInt(condkey, 0));
+      }
+    }
+
+    int mintaskcount = Integer.MAX_VALUE;
+    String mintaskid = null;
+    int mincondcount = Integer.MAX_VALUE;
+    String mincondid = null;
+    /* 
+      taskorders.forEach(function(to) { 
+        if (counts[to.id] < mintaskcount) { 
+          mintaskid = to.id;
+          mintaskcount = counts[to.id];
+        }
+      }
+      condorders.forEach(function(co) { 
+        if (counts[co.id] < mincondcount) { 
+          mincondid = co.id;
+          mincondcount = counts[co.id];
+        }
+      }
+     */
+
+    // INSERT RESERVATION FOR mintaskid AS taskrez
+    // INSERT RESERVATION FOR mincondid AS condrez
+
+    // ENQUEUE UNRESERVER FOR taskrez.getKey().getID()
+    // ENQUEUE UNRESERVER FOR condrez.getKey().getID();
+    
+    org.json.JSONObject orderings = new org.json.JSONObject();
+    // orderings["taskorder"] = orderingCache.taskorders[mintaskid];
+    // orderings["condorder"] = orderingCache.condorders[mincondid];
+    
+    orderingCache.putOpt(info.optString("sessionid"), orderings);
+    return orderings;
+  }
+
+  public class Unreserver implements com.google.appengine.api.taskqueue.DeferredTask {
+    public long rezid;
+    public Unreserver(long rezid) {
+      this.rezid = rezid;
+    }
+
+    public void run() {
+      try {
+        com.google.appengine.api.datastore.DatastoreService ds =
+          com.google.appengine.api.datastore.DatastoreServiceFactory.getDatastoreService();
+	ds.delete(com.google.appengine.api.datastore.KeyFactory.createKey("OrderStatus", rezid));	
+      } catch (Exception ex) {
+	ex.printStackTrace(System.err);
+      }
+    }
   }
 }
--- src/edu/cmu/mixer/util/ServletUtil.java
+++ src/edu/cmu/mixer/util/ServletUtil.java
@@ -3,6 +3,30 @@ package edu.cmu.mixer.util;
 import org.json.JSONObject;
 
 public class ServletUtil {
+  public static String getMD5Sum(byte[] digest) {
+    StringBuffer md5 = new StringBuffer();
+    for (byte b : digest) {
+      String s = "00" + Integer.toHexString(new Byte(b).intValue());
+      md5.append(s.substring(s.length() - 2));
+    }
+    return md5.toString();
+  }
+
+  public static String getMD5Sum(String s) throws Exception {
+    java.io.ByteArrayInputStream bais = new java.io.ByteArrayInputStream(s.getBytes());
+    java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+    java.security.DigestInputStream dis = new java.security.DigestInputStream(bais, md);
+
+    byte[] buf = new byte[1024];
+    int len = dis.read(buf);
+    while (len > 0) {
+      len = dis.read(buf);
+    }
+    dis.close();
+    bais.close();
+    return getMD5Sum(md.digest());
+  }
+  
   public static org.json.JSONObject req2json(javax.servlet.http.HttpServletRequest req) throws Exception {
     JSONObject json = new JSONObject();
 
