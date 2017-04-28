1dffdb84fa33542fe1de810cf6a689f628a511f8
Taylor Clarke <taylor.clarke13@gmail.com>
2017-04-28 15:16:40 -0400

    Finished remote api query script

--- scripts/RemoteApiQuery/RemoteApiQuery.java
+++ scripts/RemoteApiQuery/RemoteApiQuery.java
@@ -1,8 +1,17 @@
-//package com.example.appengine.remote;
+/*
+ *
+ * To run:
+ * Compile with "javac -cp '.:appengine-api.jar:appengine-remote-api.jar'  RemoteApiQuery.java"
+ * Run with "java -cp '.:appengine-api.jar:appengine-remote-api.jar'  RemoteApiQuery"
+ *
+ */
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Entity;
@@ -10,6 +19,8 @@ import com.google.appengine.tools.remoteapi.RemoteApiInstaller;
 import com.google.appengine.tools.remoteapi.RemoteApiOptions;
 
 import java.io.FileWriter;
+
+//import static org.apache.commons.lang3.StringEscapeUtils.unescapeJava;
 
 import java.io.IOException;
 
@@ -28,15 +39,62 @@ public class RemoteApiQuery
     try {
       DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
       Query query = new Query("AccessEvent");
-      query = query.addSort("timestamp",
+      query.addSort("timestamp",
                             com.google.appengine.api.datastore.Query.SortDirection.ASCENDING);
+
+      Filter dateFilter = new FilterPredicate("timestamp", FilterOperator.GREATER_THAN_OR_EQUAL, "2017");
+      query.setFilter(dateFilter);
+      
       PreparedQuery pq = ds.prepare(query);
       FetchOptions fo = FetchOptions.Builder.withDefaults();
+
+      String path = "out.csv";
+      FileWriter writer = new FileWriter(path);
+
+      writer.append("answer|condition|eventname|hash|target|taskid|taskno|timestamp\n");
+      
       int i = 0;
+      
       for (Entity e : pq.asIterable(fo)){
+        String answer = (String)e.getProperty("answer");
+        String noEscape = answer.replace("\n", "\\n");
+        noEscape = noEscape.replace("\r", "\\r");
+        noEscape = noEscape.replace("\t", "\\t");
+        //noEscape = noEscape.replace(",","\,");
+        String condition = (String)e.getProperty("condition");
+        String eventname = (String)e.getProperty("eventname");
+        String hash = (String)e.getProperty("hash");
+        String target = (String)e.getProperty("target");
+        String taskid = (String)e.getProperty("taskid");
+        String taskno = (String)e.getProperty("taskno");
+        String timestamp = (String)e.getProperty("timestamp");
+
+        writer.append(noEscape);
+        writer.append("|");
+        writer.append(condition);
+        writer.append("|");
+        writer.append(eventname);
+        writer.append("|");
+        writer.append(hash);
+        writer.append("|");
+        writer.append(target);
+        writer.append("|");
+        writer.append(taskid);
+        writer.append("|");
+        writer.append(taskno);
+        writer.append("|");
+        writer.append(timestamp);
+        writer.append("\n");
+        
+        
         i++;
-      }
+        }
+      
+      System.out.print("Number of records = ");
       System.out.println(i);
+      
+      writer.flush();
+      writer.close();
     }
     finally {
       installer.uninstall();
